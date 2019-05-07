e41c7121ff31c2d8ea529595193fc620afb0fd46
Elhamdani <spartan9452@gmail.com>
2019-05-07 10:41:53 +0200

    t

---
+++ js/app.js
@@ -0,0 +1,13 @@
+new Vue({
+    el: '#app',
+
+    data: {
+        message: 'salut les gens',
+       
+        link: 'https://www.gamekult.com/actualite/halo-pas-de-crossplay-au-lancement-de-la-master-chief-collection-sur-pc-3050816483.html',
+       
+        success: true,
+
+        persons: ['Younes', 'Joh', 'Mamadou', 'Fatima', 'Geaorge']
+    }
+})
