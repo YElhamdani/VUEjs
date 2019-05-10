400a345149ccb7259213f0c56b41b8ea862f94c6
Elhamdani <spartan9452@gmail.com>
2019-05-10 20:33:50 +0200

    d

--- js/app.js
+++ js/app.js
@@ -1,21 +1,15 @@
-new Vue({
+let vm = new Vue({
     el: '#app',
 
     data: {
-        message: 'salut les gens',
-       
-        link: 'https://www.gamekult.com/actualite/halo-pas-de-crossplay-au-lancement-de-la-master-chief-collection-sur-pc-3050816483.html',
-       
-        cls: 'success',
-
-        persons: ['Younes', 'Joh', 'Mamadou', 'Fatima', 'Geaorge']
+        seconds: 0, 
     },
 
-    methods: {
-        close: function(){
+    mounted: function(){
 
-            this.success = false;
-        },
-        
+        setInterval(() => {
+            this.seconds++
+
+        }, 1000)
     }
 })
